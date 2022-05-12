353dd4e4a336196867c92e2f5f1e3d007f11e04e
cheekslikewine <105464452+cheekslikewine@users.noreply.github.com>
2022-05-12 19:01:49 -0400

    Add files via upload

---
+++ angela.py
@@ -0,0 +1,24 @@
+
+def angela():
+    print("I'm a humongous nerd!!!")
+    print("Did I mention I also have cool rings?\n")
+
+    answer = input("Yes or No: ")
+
+    # answer = Yes
+    if answer == "Yes":
+        print("Here's a coupon that when redeemed will give you 24 hours of \
+                my very own company. I shall shower you in my \
+                undying adoration and affection. I shall feed you grapes while \
+                you lounge on a divan and I will drape you in the robes of the \
+                finest emperors. Muah")
+    elif answer == "No":
+        print("Go fuk urselph!")
+    else:
+        print("Not the answer I was looking for! Bai")
+
+def main():
+    angela()
+
+if __name__ == "__main__":
+    main()
